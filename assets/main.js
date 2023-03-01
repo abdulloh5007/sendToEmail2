748d6edc2dbf41eb1c75c11d20c24fe7a411009f
Levou7 <levou070@gmail.com>
2023-03-01 21:11:23 +0500

    new

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,56 @@
-const fname = document.querySelector('.inp1');
-const lname = document.querySelector('.inp2');
-const submit = document.querySelector('.frm');
+const frm = document.querySelector('.frm')
+const inp1 = document.querySelector('.inp1')
+const inp2 = document.querySelector('.inp2')
+const inp3 = document.querySelector('.inp3')
+const inp4 = document.querySelector('.inp4')
+const loading = document.querySelector('.loader')
+loading.style.display = 'none'
 
-submit.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    let ebody = `
-    <h1>First Name: </h1>${fname.value}
-    <br>
-    <h1>Last Name: </h1>${lname.value}
-    `;
+const body = document.querySelector('body')
 
-    Email.send({
-        SecureToken : "8ac173ba-f976-473f-a6b2-5e827305724f", //add your token here
-        To : 'abdulloh50007@gmail.com', 
-        From : "abdulloh50007@gmail.com",
-        Subject : "This is the subject",
-        Body : ebody
-    }).then(
-      message => alert(message)
-    );
-});
+const from = 'abdulloh50007@gmail.com'
+const emailBody = `<strong>From ${from}</strong> <br/>
+<b>Name: ${inp1.value}</b> <br/>
+<b>Number: ${inp3.value}</b> <br/>
+<p>Sms: ${inp4.value}</p> <br/>
+`
+
+inp1.addEventListener('keyup', (e) => {
+  if(e.target.value.length < 5){
+    inp1.style.borderBottomColor = 'red'
+  }else{
+    inp1.style.borderBottomColor = 'blue'
+  }
+})
+
+frm.addEventListener('submit', (e) => {
+  Mail()
+  e.preventDefault()
+})
+
+function Mail() {
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "abdulloh50007@gmail.com",
+    Password : "D1F934CC9A44327037D2FB13FE1EB0717534",
+    To : inp2.value,
+    From : "abdulloh50007@gmail.com",
+    Subject : 'Test',
+    Body : emailBody,
+  }).then(
+    message => alert(message)
+  );
+  setTimeout(() => {
+    frm.style.display = 'none'
+    loading.style.display = 'block'
+    body.style.backgroundColor = '#000'
+  }, 2000);
+
+  setTimeout(() => {
+    frm.style.display = 'flex'
+    loading.style.display = 'none'
+    body.style.backgroundColor = '#fff'
+    frm.reset()
+  }, 4000);
+}
+
